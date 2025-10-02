a290968b6ab0cbf50a31309358b7ba803744bd48
LauBinK <chris88wang@gmail.com>
2025-10-02 15:44:38 +0800

    Add files via upload

---
+++ pi.java
@@ -0,0 +1,9 @@
+public class pi {
+	public static void main (String[] args){
+		final double PI = 3.14159d;
+		int a = 10 ,b = 25 , c = 50;
+		System.out.println(a * a * PI);
+		System.out.println(b * b * PI);
+		System.out.println(c * c * PI);
+}
+}
